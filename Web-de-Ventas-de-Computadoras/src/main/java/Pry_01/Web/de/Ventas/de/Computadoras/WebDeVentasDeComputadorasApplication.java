167dac9895c8600f975e9214c33af37376df4225
Jesus Sebastian Magallanes Tovar <U23249740@utp.edu.pe>
2025-08-16 16:45:10 -0500

    Proyecto de Ventas

---
+++ Web-de-Ventas-de-Computadoras/src/main/java/Pry_01/Web/de/Ventas/de/Computadoras/WebDeVentasDeComputadorasApplication.java
@@ -0,0 +1,13 @@
+package Pry_01.Web.de.Ventas.de.Computadoras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDeVentasDeComputadorasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDeVentasDeComputadorasApplication.class, args);
+	}
+
+}
---
+++ Web-de-Ventas-de-Computadoras/src/test/java/Pry_01/Web/de/Ventas/de/Computadoras/WebDeVentasDeComputadorasApplicationTests.java
@@ -0,0 +1,13 @@
+package Pry_01.Web.de.Ventas.de.Computadoras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDeVentasDeComputadorasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
