7f8acebe0ac485aae53d933a79e9b48b556f989a
Jesus Sebastian Magallanes Tovar <U23249740@utp.edu.pe>
2025-09-21 00:26:48 -0500

    bk

--- Web-de-Ventas-de-Computadoras/src/main/java/Pry_01/Web/de/Ventas/de/Computadoras/Controller/ProductoController.java
+++ Web-de-Ventas-de-Computadoras/src/main/java/Pry_01/Web/de/Ventas/de/Computadoras/Controller/ProductoController.java
@@ -2,18 +2,17 @@ package Pry_01.Web.de.Ventas.de.Computadoras.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import Pry_01.Web.de.Ventas.de.Computadoras.Dto.ProductoDto;
 import Pry_01.Web.de.Ventas.de.Computadoras.Model.ProductoModel;
 import Pry_01.Web.de.Ventas.de.Computadoras.Service.ProductoService;
 import jakarta.validation.Valid;
-
 import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
@@ -31,16 +30,22 @@ public class ProductoController {
     }
 
     @PostMapping
-    public ResponseEntity<ProductoModel> crearProducto(@Valid @RequestBody ProductoModel producto){
+    public ResponseEntity<ProductoModel> crearProducto(@Valid @RequestBody ProductoModel producto) {
         ProductoModel nuevo = productoService.guardarProducto(producto);
         return ResponseEntity.ok(nuevo);
     }
 
     @DeleteMapping("/{id}")
-        public ResponseEntity<Void> eliminarProducto(@PathVariable Long id){
-            productoService.eliminarProductoPorId(id);
-            return ResponseEntity.noContent().build();
-        }
+    public ResponseEntity<Void> eliminarProducto(@PathVariable Long id) {
+        productoService.eliminarProductoPorId(id);
+        return ResponseEntity.noContent().build();
     }
 
-
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductoModel> actualizarProducto(@PathVariable Long id,
+            @Valid @RequestBody ProductoDto productoDto) {
+        return productoService.actualizarProducto(id, productoDto)
+                .map(producto -> ResponseEntity.ok().body(producto))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
