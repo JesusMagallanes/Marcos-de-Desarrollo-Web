a2dd075a22b5de31733060d13918bc88e7fdfe39
jose-257 <josefernandoab@gmail.com>
2025-10-06 17:21:55 -0500

    Logeo-Registro

--- Web-de-Ventas-de-Computadoras/src/main/java/Pry_01/Web/de/Ventas/de/Computadoras/Service/UsuarioService.java
+++ Web-de-Ventas-de-Computadoras/src/main/java/Pry_01/Web/de/Ventas/de/Computadoras/Service/UsuarioService.java
@@ -1,8 +1,8 @@
 package Pry_01.Web.de.Ventas.de.Computadoras.Service;
 
-
 import java.util.List;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import Pry_01.Web.de.Ventas.de.Computadoras.Model.UsuarioModel;
@@ -12,28 +12,41 @@ import Pry_01.Web.de.Ventas.de.Computadoras.Repository.UsuarioRepository;
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UsuarioService(UsuarioRepository usuarioRepository) {
+    public UsuarioService(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
         this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    public List<UsuarioModel> listarUsuario(){
+    public List<UsuarioModel> listarUsuario() {
         return usuarioRepository.findAll();
     }
 
-    public UsuarioModel guardarUsuario(UsuarioModel usuario){
+    public UsuarioModel guardarUsuario(UsuarioModel usuario) {
         return usuarioRepository.save(usuario);
     }
 
-    public UsuarioModel obtenerPorId(Long id){
+    public UsuarioModel obtenerPorId(Long id) {
         return usuarioRepository.findById(id).orElse(null);
     }
 
-    public void eliminarUsuario(Long id){
+    public void eliminarUsuario(Long id) {
         usuarioRepository.deleteById(id);
     }
+
     public void actualizarUsuario(UsuarioModel usuario) {
-        usuarioRepository.save(usuario); 
+        usuarioRepository.save(usuario);
     }
-   
+
+    public UsuarioModel registrarUsuario(UsuarioModel usuario) {
+        usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
+        return usuarioRepository.save(usuario);
+    }
+
+    public UsuarioModel login(String email, String password) {
+        return usuarioRepository.findByEmailAddress(email)
+                .filter(u -> passwordEncoder.matches(password, u.getPassword()))
+                .orElse(null);
+    }
 }
