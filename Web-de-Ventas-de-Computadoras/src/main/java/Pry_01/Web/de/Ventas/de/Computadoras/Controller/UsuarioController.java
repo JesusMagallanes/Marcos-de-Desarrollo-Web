4893a7ee7e1ca2dbc38754df91abdbbc67d8ba31
Jesus Sebastian Magallanes Tovar <U23249740@utp.edu.pe>
2025-10-18 21:21:17 -0500

    po

--- Web-de-Ventas-de-Computadoras/src/main/java/Pry_01/Web/de/Ventas/de/Computadoras/Controller/UsuarioController.java
+++ Web-de-Ventas-de-Computadoras/src/main/java/Pry_01/Web/de/Ventas/de/Computadoras/Controller/UsuarioController.java
@@ -1,12 +1,10 @@
 package Pry_01.Web.de.Ventas.de.Computadoras.Controller;
 
 import jakarta.servlet.http.HttpSession;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import Pry_01.Web.de.Ventas.de.Computadoras.Model.UsuarioModel;
 import Pry_01.Web.de.Ventas.de.Computadoras.Service.UsuarioService;
 
@@ -15,11 +13,9 @@ import Pry_01.Web.de.Ventas.de.Computadoras.Service.UsuarioService;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
-    private final PasswordEncoder passwordEncoder;
 
-    public UsuarioController(UsuarioService usuarioService, PasswordEncoder passwordEncoder) {
+    public UsuarioController(UsuarioService usuarioService) {
         this.usuarioService = usuarioService;
-        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping
@@ -27,27 +23,6 @@ public class UsuarioController {
         model.addAttribute("usuario", new UsuarioModel());
         model.addAttribute("usuarios", usuarioService.listarUsuario());
         return "fragments/Admin-gest/Gest-usuarios :: gest-usuarios";
-    }
-
-    @PostMapping
-    public String guardarUsuario(@ModelAttribute("usuario") UsuarioModel usuarioModel) {
-        usuarioModel.setPassword(passwordEncoder.encode(usuarioModel.getPassword()));
-        usuarioService.guardarUsuario(usuarioModel);
-        return "redirect:/VistaAdmin";
-    }
-
-    @PostMapping("/editar/{id}")
-    public String editarUsuario(@PathVariable Long id,
-            @ModelAttribute("usuario") UsuarioModel usuario) {
-        usuario.setId(id);
-        usuarioService.actualizarUsuario(usuario);
-        return "redirect:/VistaAdmin";
-    }
-
-    @PostMapping("/eliminar/{id}")
-    public String eliminarUsuario(@PathVariable Long id) {
-        usuarioService.eliminarUsuario(id);
-        return "redirect:/VistaAdmin";
     }
 
     @PostMapping("/registrar")
@@ -62,20 +37,16 @@ public class UsuarioController {
     }
 
     @PostMapping("/login")
-    public String login(@RequestParam String email,
-            @RequestParam String password,
-            HttpSession session,
-            Model model) {
+    public String login(@RequestParam String email,@RequestParam String password,HttpSession session,Model model) {
 
         UsuarioModel usuario = usuarioService.login(email, password);
 
         if (usuario != null) {
             session.setAttribute("usuario", usuario);
-
             return "redirect:/usuarios/Index-log";
         } else {
-            model.addAttribute("errorl", "Correo o contraseña incorrectos");
+            model.addAttribute("error", "Correo o contraseña incorrectos");
             return "/Index";
         }
-    }
+    }   
 }
