82eab33432c581c0fde5258cbb88941b17acc629
Salin Fabrizio Flores Condori <u23250409@utp.edu.pe>
2025-10-29 17:48:06 -0500

    asd

--- Web-de-Ventas-de-Computadoras/src/main/java/Pry_01/Web/de/Ventas/de/Computadoras/Controller/IndexController.java
+++ Web-de-Ventas-de-Computadoras/src/main/java/Pry_01/Web/de/Ventas/de/Computadoras/Controller/IndexController.java
@@ -7,53 +7,49 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import Pry_01.Web.de.Ventas.de.Computadoras.Model.CategoriaModel;
+import Pry_01.Web.de.Ventas.de.Computadoras.Dto.ProductosDTO.ProductosResponseDTO;
 import Pry_01.Web.de.Ventas.de.Computadoras.Service.CategoriaService;
+import Pry_01.Web.de.Ventas.de.Computadoras.Service.ProductoService;
 
 @Controller
 public class IndexController {
-    private final CategoriaService categoriaService;
 
-    public IndexController(CategoriaService categoriaService) {
+    private final CategoriaService categoriaService;
+    private final ProductoService productoService;
+
+    public IndexController(CategoriaService categoriaService, ProductoService productoService) {
         this.categoriaService = categoriaService;
+        this.productoService = productoService;
     }
 
-//   @GetMapping("/")
-//    public String Principal() {
-//        return "redirect:/Index";
-//    }
-
-//    @GetMapping("/{view}")
-//    public String page(@PathVariable String view) {
-//       for (String u : USER) {
-//           if (u.equals(view))
-//              return view;
-//     }
-//     return "redirect:/Index";
-//   }
-
-    
     @GetMapping("/Index")
     public String mostrarIndex(Model model) {
         List<CategoriaModel> categorias = categoriaService.listarCategoria();
+        List<ProductosResponseDTO> productos = productoService.listarProducto();
+
         model.addAttribute("categorias", categorias);
+        model.addAttribute("productos", productos);
+
         return "Index";
     }
 
     @GetMapping("/header")
     public String mostrarHeader() {
-    return "fragments/headerFooter/header";
+        return "fragments/headerFooter/header";
     }
 
     @GetMapping("/Carrito")
     public String mostrarCarrito() {
-    return "Carrito";
+        return "Carrito";
     }
+
     @GetMapping("/metodosPago")
-    public String metodosPago() { return "metodosPago"; }
-    
+    public String metodosPago() {
+        return "metodosPago";
+    }
+
     @GetMapping("/productosCategoria")
-    public String mostrarProductoCategoria(){
+    public String mostrarProductoCategoria() {
         return "productosCategoria";
     }
 }
-
