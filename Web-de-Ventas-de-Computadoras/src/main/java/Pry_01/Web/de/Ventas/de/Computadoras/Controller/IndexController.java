b34c62f9860837dd5dbb35ecbeb2ae9a7d7ae58f
Jesus Sebastian Magallanes Tovar <U23249740@utp.edu.pe>
2025-10-30 13:34:14 -0500

    awa de uwu

--- Web-de-Ventas-de-Computadoras/src/main/java/Pry_01/Web/de/Ventas/de/Computadoras/Controller/IndexController.java
+++ Web-de-Ventas-de-Computadoras/src/main/java/Pry_01/Web/de/Ventas/de/Computadoras/Controller/IndexController.java
@@ -1,7 +1,6 @@
 package Pry_01.Web.de.Ventas.de.Computadoras.Controller;
 
 import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,7 +20,7 @@ public class IndexController {
     private final ProductoService productoService;
 
     // Inyección por constructor (recomendada)
-    public IndexController(CategoriaService categoriaService) {
+    public IndexController(CategoriaService categoriaService, ProductoService productoService) {
         this.categoriaService = categoriaService;
         this.productoService = productoService;
     }
