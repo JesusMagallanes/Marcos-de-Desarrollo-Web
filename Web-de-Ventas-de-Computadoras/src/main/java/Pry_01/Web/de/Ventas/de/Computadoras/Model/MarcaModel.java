4d4bf3a6433f53d963598af918a6addbeacfc9ed
jose-257 <u23272081@utp.edu.pe>
2025-12-05 12:54:51 -0500

    Revert "la fe"
    This reverts commit 93c675ea5fdea4e29752debff794a8e2a9309f30.

--- Web-de-Ventas-de-Computadoras/src/main/java/Pry_01/Web/de/Ventas/de/Computadoras/Model/MarcaModel.java
+++ Web-de-Ventas-de-Computadoras/src/main/java/Pry_01/Web/de/Ventas/de/Computadoras/Model/MarcaModel.java
@@ -3,15 +3,18 @@ package Pry_01.Web.de.Ventas.de.Computadoras.Model;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,12 +31,26 @@ public class MarcaModel {
     private Long id;
 
     @NotBlank
+    @Size(max = 100)
+    @Column(nullable = false, length = 100)
     private String nombre;
 
     @ManyToOne
-    @JoinColumn(name = "categoria_id")
+    @JoinColumn(name = "categoria_id", nullable = false)
     private CategoriaModel categoria;
 
-    @ManyToMany(mappedBy = "marcas")
+    @OneToMany(mappedBy = "marca", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ProductoModel> productos = new ArrayList<>();
+
+    public void addProducto(ProductoModel producto) {
+        productos.add(producto);
+        producto.setMarca(this);
+    }
+
+    public void removeProducto(ProductoModel producto) {
+        productos.remove(producto);
+        producto.setMarca(null);
+    }
 }
+
+
